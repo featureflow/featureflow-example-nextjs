75aefb547ceb6d568e16e71b5fb0b980486fa11e
Oliver Oldfield-Hodge <oliver@oldfield-hodge.com>
2019-03-07 00:25:30 +1100

    Example of both client and server-side rendering in next.js using the nodejs featureflow SDK

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,52 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
-//import Featureflow from 'featureflow-node-sdk';
 
 const Index = (props) => (
   <Layout>
-    <h1>Batman TV Shows</h1>
+    <h1>Features</h1>
     <ul>
-      {props.shows.map(({show}) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
+      {props.featureKeys.map((key, index) => (
+        <li key={key}>
+            {key} is {props.features[key]}
         </li>
       ))}
     </ul>
   </Layout>
 )
 
-Index.getInitialProps = async function() {
+Index.getInitialProps = async function({req}) {
 
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-  const data = await res.json()
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+    let features= {};
+    let featureKeys = [];
+    if( req ){
+        features = req.features;
+        for (let p in features) {
+            if(  features.hasOwnProperty(p) ) {
+                featureKeys.push(p);
+            }
+        }
+    }else{
+        const res = await fetch('/api/features')
+        features = await res.json()
+        for (let p in features) {
+            if(  features.hasOwnProperty(p) ) {
+                featureKeys.push(p);
+            }
+        }
+    }
 
-  console.log(`Show data fetched. Count: ${data.length}`)
+    console.log(`Evaluated features ${features}`)
+    console.log(`Show data fetched. Count: ${data.length}`)
 
   return {
-    shows: data
+    shows: data,
+    features: features,
+    featureKeys: featureKeys
   }
-}
+};
 
 
 
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+
 const express = require('express')
 const next = require('next')
+const Featureflow = require('../../featureflow-sdks/featureflow-node-sdk');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
+const featureflow = new Featureflow.Client({apiKey: 'srv-env-685e066dea464f88be14effbf65cf69c'});
 app.prepare()
 .then(() => {
   const server = express()
+    server.use((req, res, next) => {
 
-  server.get('/p/:id', (req, res) => {
-      const actualPage = '/post'
-      const queryParams = { id: req.params.id }
-      app.render(req, res, actualPage, queryParams)
-  })
+        let user = new Featureflow.UserBuilder("user1")
+            .withAttribute('country', 'US')
+            .withAttribute('page', 'index')
+            .withAttributes('roles', ['USER_ADMIN', 'BETA_CUSTOMER'])
+            .build();
+
+        req.features = featureflow.evaluateAll(user);
+        return next();
+    })
+
+    server.get('/api/features', (req, res) => {
+        let user = new Featureflow.UserBuilder("user2")
+            .withAttribute('country', 'US')
+            .withAttribute('page', 'post')
+            .withAttributes('roles', ['USER_ADMIN', 'BETA_CUSTOMER'])
+            .build();
+        res.json(featureflow.evaluateAll(user));
+    })
+
   server.get('*', (req, res) => {
     return handle(req, res)
   })
